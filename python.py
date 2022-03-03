87bedd7b389b4ceb2360220be26bbe1435ed742c
Unknowncoder2003 <100753526+Unknowncoder2003@users.noreply.github.com>
2022-03-03 18:18:17 +0700

    Create python.py

---
+++ python.py
@@ -0,0 +1,12 @@
+import turtle
+wn = turtle.Screen()
+wn.title("calculator")
+wn.bgcolor("black")
+wn.setup(width=800, height=600 )
+wn.tracer(0)
+
+x = int(input())
+y = int(input())
+symbol = input()
+if symbol == "+":
+
